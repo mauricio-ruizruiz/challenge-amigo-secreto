0c0de03770c8f5b1807f3daadf4fbe3b0d212b1f
Mauricio Ruiz <pixel.balam@gmail.com>
2025-02-03 23:56:39 -0600

    Ejercicio Finalizado

---
+++ README.md
@@ -0,0 +1,22 @@
+# Sorteo de Amigos
+
+Este proyecto es una simple aplicación web que permite gestionar una lista de amigos y sortear aleatoriamente uno de los nombres de la lista.
+
+## Características
+
+- Agregar una lista de amigos a una lista desordenada (`<ul>`).
+- Sortear un nombre aleatorio de la lista.
+- Mostrar el resultado en la página.
+
+## Tecnologías Utilizadas
+
+- **HTML**
+- **JavaScript**
+- **CSS**
+
+## Uso
+
+1. Abrir `index.html` en un navegador.
+2. Presionar **"Actualizar Lista"** para cargar los nombres.
+3. Presionar **"Sortear Amigo"** para seleccionar aleatoriamente un amigo.
+4. Ver el resultado en la página.
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+  let amigoUsuario = document.getElementById("amigo").value;
+
+  if (amigoUsuario === "") {
+    alert("Por favor, inserte un nombre");
+  } else if (amigoUsuario !== "") {
+    amigos.push(amigoUsuario);
+  }
+  //   console.log(amigoUsuario);
+  //   console.log(typeof amigoUsuario);
+  console.log(amigos);
+  return;
+}
+
+function actualizaAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
+
+function limpiarValor() {
+  document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  const resultado = document.getElementById("resultado");
+
+  if (amigos.length === 0) {
+    resultado.innerHTML = "No hay amigos en la lista para sortear.";
+    return;
+  } else {
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `El amigo secreto sorteado es: <strong>${amigoSorteado}</strong>`;
+  }
+}
